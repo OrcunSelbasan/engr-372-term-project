f42334f886868e8484ffb05c888d1cdf5e68d09b
Orcun Selbasan <lutfuorcunselbasan@gmail.com>
2023-11-17 18:07:31 +0300

    Changed code alignment, added todo task comment

--- index.php
+++ index.php
@@ -16,6 +16,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="./css/index.css">
         <script src="./jquery/jquery-3.7.1.js"></script>
+        <!-- TODO: UPDATE THE TITLE -->
         <title>Document</title>
     </head>
     <body>
--- view/storage.php
+++ view/storage.php
@@ -12,6 +12,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <script src="./jquery/jquery-3.7.1.js"></script>
         <link rel="stylesheet" href="../css/index.css">
+        <!-- TODO: UPDATE THE TITLE -->
         <title>Document</title>
     </head>
     <body>
