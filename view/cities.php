09bd3914609a44792d53b363e7aefe872a35d644
glimo44 <f.moeglich@gmx.de>
2023-12-12 09:10:41 +0300

    prototype only html

--- view/cities.php
+++ view/cities.php
@@ -1,21 +1,25 @@
 <?php
-    include("../controller/ControllerAuth.php");
-    // * Check if the user is authenticated
-    $auth = new ControllerAuth();
-    $auth->checkAuth();
+include("../controller/ControllerAuth.php");
+// * Check if the user is authenticated
+$auth = new ControllerAuth();
+$auth->checkAuth();
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <script src="./jquery/jquery-3.7.1.js"></script>
-        <link rel="stylesheet" href="../css/index.css">
-        <!-- TODO: UPDATE THE TITLE -->
-        <title>Document</title>
-    </head>
-    <body>
-        <?php include("./header.php"); ?>
-    </body>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="./jquery/jquery-3.7.1.js"></script>
+    <link rel="stylesheet" href="../css/index.css">
+    <title>Cities</title>
+</head>
+
+<body>
+    <?php include("./header.php"); ?>
+    <?php include("./index_cities.html"); ?>
+
+</body>
+
 </html>
