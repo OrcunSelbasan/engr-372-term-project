9fc626ea407196ae348381c0bdb896ab97e734fe
Orcun Selbasan <lutfuorcunselbasan@gmail.com>
2023-12-17 22:53:28 +0300

    lets use common credentials

--- utils/constants.php
+++ utils/constants.php
@@ -2,7 +2,7 @@
 class Constants
 {
     const DB_HOST = "localhost";
-    const DB_USER = "orcun";
+    const DB_USER = "engradmin";
     const DB_PASSWORD = "1234qwer";
     const DB_NAME = "group4";
 }
